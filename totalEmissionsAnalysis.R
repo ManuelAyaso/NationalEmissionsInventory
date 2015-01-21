9ad9833d908a430300a8924cbb5eb1f6009995d3
Manuel Ayaso <manuel.ayaso.enriquez@gmail.com>
2015-01-21 19:03:40 +0000

    Save total emissions as png

--- totalEmissionsAnalysis.R
+++ totalEmissionsAnalysis.R
@@ -8,6 +8,8 @@ source("loadData.R")
 # Total emissions grouped by year
 totalEmissionsByYear <- ddply(NEI, c("year"), summarize, Emissions = sum(Emissions))
 
+png("images/totalEmissions.png")
+
 par(mar = c(5, 5, 2, 2))
 plot(totalEmissionsByYear$year, totalEmissionsByYear$Emissions, type = "b", 
      xlab = "Year", ylab = "Total Emissions", pch = 20, 
@@ -17,3 +19,5 @@ abline(h = 7000000, untf = FALSE, col = "red")
 text(2003.5, 7300000, "7,000,000", col = "blue")
 abline(h = 3500000, untf = FALSE, col = "green")
 text(2003.5, 3800000, "3,500,000", col = "blue")
+
+dev.off()
