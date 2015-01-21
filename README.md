959d38bcaa6e8af84091b11b9fbfc1edf9259483
Manuel Ayaso <manuel.ayaso.enriquez@gmail.com>
2015-01-21 18:24:41 +0000

    Answer to the first question

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # National Emissions Inventory Project
 Main repository for the Course Project 2 for Exploratory Data Analysis in Coursera
+
+I'll answer the 6 questions for this assignment using plots.
+
+1. Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+
+They have clearly decreased as we can see in the plot. We can see that the went from more than 7 million in 1999 to less than 3 and a half million in 2008.
+
+
--- totalEmissionsAnalysis.R
+++ totalEmissionsAnalysis.R
@@ -1,1 +1,15 @@
-# Have total emissions increased from 1999 to 2008?
+# Have total emissions decreased from 1999 to 2008?
+
+require(plyr)
+
+# Load data into R Environment (variables NEI and SCC)
+source("loadData.R")
+
+# Total emissions grouped by year
+totalEmissionsByYear <- ddply(NEI, c("year"), summarize, Emissions = sum(Emissions))
+
+par(mar = c(5, 5, 2, 2))
+plot(totalEmissionsByYear$year, totalEmissionsByYear$Emissions, type = "b", 
+     xlab = "Year", ylab = "Total Emissions", pch = 20, 
+     xaxt = "n", ylim = c(0, max(totalEmissionsByYear$Emissions)))
+axis(1, at = seq(1999, 2008, by = 1), las = 2)
